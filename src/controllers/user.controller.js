e20b47ca5b0cb18d3b76866822d21ab9617fbf67
Aditya Bhoite <bhoiteaditya29@gmail.com>
2026-09-25 20:31:14 +0530

    add watch history aggregation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -381,7 +381,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         )
 })
 
-const getUserChanneProfile = asyncHandler(async (req, res) => {
+const getUserChannelProfile = asyncHandler(async (req, res) => {
     const { username } = req.params //req.params is used to identify a specific resource using the URL
 
     if (!username?.trim()) {
@@ -451,6 +451,57 @@ const getUserChanneProfile = asyncHandler(async (req, res) => {
             new APIresponse(200, channel[0], "user channel fetched successfully")
         )
 })
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    res
+        .status(200)
+        .json(
+            new APIresponse(200, user[0], watchHistory, "watch history fetched successfully")
+        )
+})
+
 export {
     registerUser,
     loginUser,
@@ -461,5 +512,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChanneProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -33,11 +45,15 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 
 router.route("/get-user").get(verifyJWT, getCurrentUser)
 
-router.route("/update-acc-details").post(verifyJWT, updateAccountDetails)
+router.route("/update-acc-details").patch(verifyJWT, updateAccountDetails)
 
-router.route("/update-user-avatar").post(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-user-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
-router.route("/update-user-cover").post(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/update-user-cover").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/get-watch-history").get(verifyJWT, getWatchHistory)
 
 
 export default router
